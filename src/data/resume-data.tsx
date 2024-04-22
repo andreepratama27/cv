533b98828422759e8b00140d35e957a207f4ee4b
andre7131041 <andre@primaku.com>
2024-04-22 15:35:26 +0700

    feat: modify cv

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,163 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
-  about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+  name: "Andre Pratama",
+  initials: "AP",
+  location: "Medan, Indonesia",
+  locationLink: "https://www.google.com/maps/place/Medan",
+  about: "Senior Frontend Engineer",
   summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    `Experienced Frontend Engineer with 6 years of expertise in JavaScript, specializing in React & Next.js.
+Demonstrated success in driving revenue growth and delivering high-quality products. Proficient in
+leading small frontend teams, fostering collaboration, and prioritizing professional development. Known
+for strong communication skills and a team-oriented approach. Eager to continually learn and adapt to
+new tools and technologies in web development.`,
+  avatarUrl: "https://media.licdn.com/dms/image/C5103AQFJP1k6HF0hPw/profile-displayphoto-shrink_400_400/0/1575032726714?e=1719446400&v=beta&t=_jVjSuzKdIuF8RbVBX7lmqs3sfRrr9JXL2lEpCi8wdU",
+  personalWebsiteUrl: "https://andrepratama.dev",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "andreepratama27@gmail.com",
+    tel: "+6285156847850",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/andreepratama27",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/andre-pratama27/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/andreepratamaa",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "STMIK Mikroskil",
+      degree: "Bachelor's Degree in Informatics engineering. GPA: 3,41",
+      start: "2013",
+      end: "2017",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Primaku",
+      link: "https://primaku.com",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      title: "Senior Frontend Engineer",
       logo: ParabolLogo,
-      start: "2021",
-      end: "2024",
+      start: "2023",
+      end: "Now",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        `Leading Frontend Team on user facing product, initiate sprint-grooming for web team, take a technical decision for tools & technolgy that implemented on the project. Technologies: React,
+        Next JS, Typescript, Tailwind CSS`,
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
+      company: "Stockbit",
+      link: "https://stockbit.com",
       badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
+      title: "Senior Frontend Engineer",
       logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      start: "2022",
+      end: "2023",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+      `Created, Developed and maintain Stockbit Malaysia Platform, contribute to improve Ebook performance on Stockbit Platform, contribute to growth company revenue by implementing Paywall Feature. Technologies:
+      React, Next JS, Typescript, Styled-Components, Cypress
+      `
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
+      company: "Accelbyte, Inc",
+      link: "https://accelbyte.io",
+      badges: ["Onsite (Yogyakarta, Indonesia)"],
+      title: "Frontend Engineer",
       logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      start: "2020",
+      end: "2022",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
+        `Collaborating with Backend Engineer, QA and Product Owner to develop and
+maintain Armada, a game server web based app for multiplayer game purpose, acting as a lead frontend, take initiative and decision about tools & library that used on the project. Technologies: React, Typescript, CSS Modules, Docusaurus`,
     },
     {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
+      company: "Pomona",
+      link: "https://pomona.id",
+      badges: ["Onsite (Jakarta, Indonesia)"],
+      title: "Frontend Engineer",
       logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+      start: "2019",
+      end: "2020",
+      description: `Join on B2B team to create Verifier Dashboard, develop & maintain PWA Web App, develop & maintain campaign mobile app for custom order. Technologies: React, React Native, GraphQL, Typescript`,
+    },
+    {
+      company: "TrackerHero",
+      link: "https://trackerhero.com",
+      badges: ["Onsite - Cyberjaya, Selangor (Malaysia)"],
+      title: "Mobile Engineer (React Native)",
+      logo: NSNLogo,
+      start: "2017",
+      end: "2019",
+      description: `Initiate, Create & Develop Tracking Patrol App with custom devices, implement & maintain native-modules for custom mobile apps, provide testing & training with operation teams. Technologies:
+      React Native, Typescript, Redux Thunk, Redux Saga, Material UI`,
     },
   ],
   skills: [
     "JavaScript",
     "TypeScript",
     "React/Next.js/Remix",
+    "React Native",
     "Node.js",
     "GraphQL",
-    "Relay",
-    "WebRTC",
+    "Tailwind CSS",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Stockbit Malaysia Platform",
       techStack: [
-        "Side Project",
-        "TypeScript",
         "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "TypeScript",
+        "Styled Components",
+        "Cypress",
       ],
-      description: "A platform to build and grow your online business",
+      description: "A Social Media for Stock Information build for Malaysia Users",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "stockbit.my",
+        href: "https://stockbit.my",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
-      description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
-      link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
-      },
-    },
-    {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
-      },
-    },
-    {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
-      description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
-      link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
-      },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
+      title: "Primaku Website",
       techStack: [
-        "Full Stack Developer",
+        "Next.js",
         "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
+        "Tailwind CSS",
+        "Redux Toolkit",
+        "PWA"
       ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+      description: "A Primaku Website",
+      logo: ConsultlyLogo,
       link: {
-        label: "github.com",
-        href: "https://parabol.co/",
+        label: "primaku-web",
+        href: "primaku.com",
       },
     },
     {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
-    },
+title: "Primaku Parenthood Institute 2023",
+       techStack: [
+         "Next.js",
+       "TypeScript",
+       "Tailwind CSS",
+       "Zustand",
+       ],
+       description: "An Education Platform for Baby & Child Growth",
+       logo: ConsultlyLogo,
+       link: {
+label: "primaku-parenthood",
+       href: "app-parenthood.primaku.com",
+       },
+    } 
   ],
 } as const;
